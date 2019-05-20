38d8c6e81ef2049ca278c728bd41de7f07adeff7
unknown <fauzimakarim123@gmail.com>
2019-05-20 22:37:48 +0700

    template backend

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -47,10 +47,11 @@ class SignupForm extends Model
         if (!$this->validate()) {
             return null;
         }
-        
+
         $user = new User();
         $user->username = $this->username;
         $user->email = $this->email;
+        $user->status = 10;
         $user->setPassword($this->password);
         $user->generateAuthKey();
         $user->generateEmailVerificationToken();
